deeb00ccef5238cf8138a6cdd45c93cac3d2de32
betascribbles <firdaushassan033@gmail.com>
2022-04-04 22:44:38 +0300

    corrected linting

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,16 +1,16 @@
-import getBudgetObject from "./7-getBudgetObject";
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
-	const budget = getBudgetObject(income, gdp, capita);
-	const fullBudget = {
-		...budget,
-		getIncomeInDollars(income) {
-			return `$${income}`;
-		},
-		getIncomeInEuros(income) {
-			return `${income} euros`;
-		},
-	};
+  const budget = getBudgetObject(income, gdp, capita);
+  const fullBudget = {
+    ...budget,
+    getIncomeInDollars(income) {
+      return `$${income}`;
+    },
+    getIncomeInEuros(income) {
+      return `${income} euros`;
+    },
+  };
 
-	return fullBudget;
+  return fullBudget;
 }
