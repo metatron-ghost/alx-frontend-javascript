a13690a7507b83f8412bdba355381f0e418afea5
betascribbles <firdaushassan033@gmail.com>
2022-04-06 23:19:21 +0300

    adv task

---
+++ 0x01-ES6_promise/100-await.js
@@ -0,0 +1,9 @@
+import { uploadPhoto, createUser } from "./utils";
+
+export default async function asyncUploadUser() {
+	try {
+		return { photo: await uploadPhoto(), user: await createUser() };
+	} catch (e) {
+		return { photo: null, user: null };
+	}
+}
