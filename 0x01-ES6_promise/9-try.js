66e22516a6cf8083e0fee355a017158de547e559
betascribbles <firdaushassan033@gmail.com>
2022-04-06 23:17:25 +0300

    task 9

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,9 @@
+export default function guardrail(mathFunction) {
+	const queue = [];
+	try {
+		queue.push(mathFunction(), "Guardrail was processed");
+	} catch (err) {
+		queue.push(`Error: ${err.message}`, "Guardrail was processed");
+	}
+	return queue;
+}
